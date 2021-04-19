b3fc00efff9fe003b652d60ac98730c6d398e107
GeorgiPetrov8 <69316319+GeorgiPetrov8@users.noreply.github.com>
2021-04-19 16:22:58 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_rest_books/SpringRestBooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_rest_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestBooksApplication.class, args);
+    }
+
+}
